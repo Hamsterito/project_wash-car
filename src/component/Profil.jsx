af66125ae819e8bb33eb58b1be61d33f2eeccc8a
Shyngys <abildin.shyngys17@gmail.com>
2025-04-24 20:49:46 +0500

    profile

--- src/component/Profil.jsx
+++ src/component/Profil.jsx
@@ -1,11 +1,26 @@
-
+import './Profil.css';
 import React from 'react';
 
 const Profil = () => {
   return (
     <div>
-      <h1>О нас</h1>
-      <p>Это страница "О нас".</p>
+      <div>
+        <h2>Личный кабинет</h2>
+        <p><strong>Имя:</strong> Иван</p>
+        <p><strong>Фамилия:</strong> Иванов</p>
+        <p><strong>Номер:</strong> +7 (123) 456-78-90</p>
+        <p><strong>Email:</strong> ivanov@example.com</p>
+      </div>
+      <div>
+        <img 
+          src="src\assets\Rectangle163.jpg"
+          alt="Фото профиля" 
+        />
+      </div>
+      <div>
+        <button>Изменить фото</button>
+          <button className='buttonLogOut'>Log Out</button>
+      </div>
     </div>
   );
 };
