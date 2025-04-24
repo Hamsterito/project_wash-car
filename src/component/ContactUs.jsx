c90209918aad9328e287fc2b5b3baa4013fdcc43
Hamsterito <kabdrashev111@gmail.com>
2025-04-24 14:32:40 +0500

    add_contact

--- src/component/ContactUs.jsx
+++ src/component/ContactUs.jsx
@@ -6,20 +6,21 @@ const ContactUs = () => {
     email: "",
   });
 
-
   return (
-    <div class="help">
-        <h1 className="need_help_text">Нужна помощь?</h1>
-        <p className="contact_us">Свяжитесь<br/>
-            с нами! 
-            </p>
-        <div class="сontacts">
-            <h1>Наши контакты</h1>
-            <p>+7 777 001 57 43</p>
-            <p>kabdrashev111@gmail.com</p>
+    <div className="help">
+      <div className="overlay_contact">
+        <div className="help_text">
+          <h1 className="need_help_text">Нужна помощь?</h1>
+          <p className="contact_us">Свяжитесь с нами!</p>
         </div>
+        <div className="contacts">
+          <h1>Наши контакты</h1>
+          <p>- +7 777 001 57 43</p>
+          <p>- kabdrashev111@gmail.com</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default ContactUs;
