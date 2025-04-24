dfba217680b056938a36ff90a69f1e67c123cf4b
Hamsterito <kabdrashev111@gmail.com>
2025-04-24 15:59:49 +0500

    change

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,15 +2,28 @@ import React, { useState } from 'react';
 import './Header.css';
 import AuthModal from './AuthModal';
 import userIcon from '../assets/user-icon.png';
+<<<<<<< HEAD
 import logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
+=======
+import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+
+
+>>>>>>> origin/chin
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [showAuth, setShowAuth] = useState(false);
 
+  const navigate = useNavigate();
+
   const handleProfileClick = () => {
+<<<<<<< HEAD
     window.location.href = "/account";
+=======
+    navigate('/account');
+>>>>>>> origin/chin
   };
 
   return (
