2a3964daf836039acf85f6137ae801dd2be3a99c
Hamsterito <kabdrashev111@gmail.com>
2025-04-24 16:21:49 +0500

    fix:исправил header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,36 +1,23 @@
-import React, { useState } from 'react';
-import './Header.css';
-import AuthModal from './AuthModal';
-import userIcon from '../assets/user-icon.png';
-<<<<<<< HEAD
-import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
-=======
-import logo from '../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
-
-
->>>>>>> origin/chin
+import React, { useState } from "react";
+import "./Header.css";
+import AuthModal from "./AuthModal";
+import userIcon from "../assets/user-icon.png";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showAuth, setShowAuth] = useState(false);
 
-  const navigate = useNavigate();
-
   const handleProfileClick = () => {
-<<<<<<< HEAD
     window.location.href = "/account";
-=======
-    navigate('/account');
->>>>>>> origin/chin
   };
 
   return (
     <>
       <header className="header">
         <div className="logo">
-          <img src={logo} alt="Логотип" /> 
+          <img src={logo} alt="Логотип" />
         </div>
 
         <nav className="nav">
@@ -40,16 +27,15 @@ const Header = () => {
         </nav>
         <div className="auth-buttons">
           {isLoggedIn ? (
-              <Link to="/profile">
-                <img
-                  src={userIcon}
-                  alt="User Icon"
-                  className="user-icon"
-                />
-              </Link>
+            <img
+              src={userIcon}
+              alt="User Icon"
+              className="user-icon"
+              onClick={handleProfileClick}
+            />
           ) : (
             <button
-              className="booking-button"
+              className="auto-button"
               onClick={() => setShowAuth(true)}
             >
               Вход / Регистрация
