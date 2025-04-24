0bcb958f580bd839f82bb3b4bbe762cae2fef2bd
Hamsterito <kabdrashev111@gmail.com>
2025-04-24 15:59:49 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import BannerSlider from "./component/BannerSlider";
 import Footer from "./component/Footer";
 import CitySelector from './component/CitySelector';
 import CarWashGrid from './component/CarWashGrid';
-import AboutUs from './component/AboutUs';
-import Profil from './component/Profil'; 
-import ContactUs from "./component/ContactUs";
-// 
+
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <BannerSlider />
-            <CitySelector />
-            <CarWashGrid />
-            <AboutUs />
-            <ContactUs />
-            <Footer />
-          </>
-        } />
-        <Route path="/profile" element={<Profil />} />
-      </Routes>
-    </Router>
+    <div>
+      <Header/>
+      <BannerSlider/>
+      <CitySelector />
+      <CarWashGrid />
+      <Footer/>
+    </div>
   );
 };
 
