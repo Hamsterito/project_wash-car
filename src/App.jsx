b26242b2303656afc6b4c66cf514a537b9822a96
Your Name <you@example.com>
2025-04-25 13:30:13 +0500

    add profil

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./component/Header";
 import BannerSlider from "./component/BannerSlider";
 import Footer from "./component/Footer";
 import CitySelector from './component/CitySelector';
 import CarWashGrid from './component/CarWashGrid';
-
+import AboutUs from './component/AboutUs';
+import Profil from './component/Profil'; 
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <BannerSlider/>
-      <CitySelector />
-      <CarWashGrid />
-      <Footer/>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <BannerSlider />
+            <CitySelector />
+            <CarWashGrid />
+            <AboutUs />
+            <Footer />
+          </>
+        } />
+        <Route path="/profile" element={<Profil />} />
+      </Routes>
+    </Router>
   );
 };
 
