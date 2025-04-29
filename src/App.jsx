7c52e237ae40e74e6f98f9ab0cd43c31baba55d3
Hamsterito <kabdrashev111@gmail.com>
2025-04-29 23:31:06 +0500

    структуру поменять добавил pages - что бы руты нормально работали

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Header from "./component/Header";
-import BannerSlider from "./component/BannerSlider";
-import Footer from "./component/Footer";
-import CitySelector from './component/CitySelector';
-import CarWashGrid from './component/CarWashGrid';
-import AboutUs from './component/AboutUs';
-import ContactUs from './component/ContactUs';
-import ProfilePage from './component/ProfilePage'; 
+import ProfilePage from "./component/ProfilePage";
+import MainPage from "./pages/MainPage";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={
-          <>
-            <BannerSlider />
-            <CitySelector />
-            <CarWashGrid />
-            <AboutUs />
-            <ContactUs />
-            <Footer />
-          </>
-        } />
+        <Route path="/" element={<MainPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import BannerSlider from "../component/BannerSlider";
+import CitySelector from "../component/CitySelector";
+import CarWashGrid from "../component/CarWashGrid";
+import AboutUs from "../component/AboutUs";
+import ContactUs from "../component/ContactUs";
+import Footer from "../component/Footer";
+
+const MainPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      const el = document.getElementById(location.state.scrollTo);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return (
+    <>
+      <BannerSlider />
+      <CitySelector />
+      <div id="car-wash-grid">
+        <CarWashGrid />
+      </div>
+      <div id="about">
+        <AboutUs />
+      </div>
+      <ContactUs />
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
