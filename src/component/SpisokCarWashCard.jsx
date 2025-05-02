321064bb8581b523a9a6b58a6047c72ff2226a0d
Your Name <you@example.com>
2025-05-03 02:53:00 +0500

    CarWashSchedule

--- src/component/SpisokCarWashCard.jsx
+++ src/component/SpisokCarWashCard.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import './SpisokCarWashCard.css'; 
+import { useNavigate } from "react-router-dom";
+import "./SpisokCarWashCard.css"
 
-const SpisokCarWashCard = ({ name, address, imageUrl, onClick }) => {
+const SpisokCarWashCard = ({ id, name, address, image }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/carwash/${id}`);
+  };
+
   return (
-    <button className="carwash-card" onClick={() => onClick(name)}>
+    <button className="carwash-card" onClick={handleClick}>
       <div className="carwash-info">
         <h3 className="carwash-name">{name}</h3>
         <p className="carwash-address">{address}</p>
       </div>
-      <img className="carwash-image" src={imageUrl} alt="Автомойка" />
+      <img className="carwash-image" src={image} alt="Автомойка" />
     </button>
   );
 };
